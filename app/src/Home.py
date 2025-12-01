9f9fe700ab65d30aecb3d73205ec379828d64d0c
alantai26 <tai.a@northeastern.edu>
2025-12-01 09:05:28 -0500

    fixed minor bug

--- app/src/Home.py
+++ app/src/Home.py
@@ -50,20 +50,20 @@ if st.button("Act as James, a 2nd year applying for this co-op cycle!",
     # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
     # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
+    st.session_state['role'] = 'applicant'
     # we add the first name of the user (so it can be displayed on 
     # subsequent pages). 
-    st.session_state['first_name'] = 'John'
+    st.session_state['first_name'] = 'James'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+    logger.info("Logging in as an Applicant Persona")
+    st.switch_page('pages/00_Applicant_Home.py')
 
 if st.button('Act as Marcus, a career coach helping students with their applications!', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
+    st.session_state['role'] = 'career_couch'
     st.session_state['first_name'] = 'Mohammad'
     st.switch_page('pages/10_USAID_Worker_Home.py')
 
